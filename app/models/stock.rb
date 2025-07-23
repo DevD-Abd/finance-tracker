72958e81d9e761a7ff8635c7f6e3960fb612df3f
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-23 05:59:14 -0400

    stock users association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
   validates :ticker, presence: true, uniqueness: true
 
   # Stock data with realistic company names (for fallback only)
