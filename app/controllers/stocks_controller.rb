4334a570ac27d8dbdce163617f8956c95e0e1b55
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-23 06:38:34 -0400

    user specific stocks and browse all available stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,51 @@
 class StocksController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
-    @stocks = Stock.all.order(:ticker)
+    @stocks = current_user.stocks.order(:ticker)
+  end
+
+  def browse
+    @search_query = params[:search]
+    
+    if @search_query.present?
+      # Search in existing stocks first - using SQLite compatible case-insensitive search
+      @stocks = Stock.where("UPPER(ticker) LIKE ? OR UPPER(name) LIKE ?", "%#{@search_query.upcase}%", "%#{@search_query.upcase}%")
+      
+      # If no results found, try to fetch from Alpha Vantage API to validate the ticker
+      if @stocks.empty?
+        ticker = @search_query.upcase.strip
+        
+        # Try to fetch from Alpha Vantage API regardless of format
+        potential_stock = Stock.update_or_create_from_alpha_vantage(ticker)
+        
+        if potential_stock
+          @stocks = [potential_stock]
+          flash.now[:notice] = "Found and added #{ticker} from Alpha Vantage API"
+        else
+          flash.now[:alert] = "No stock data found for '#{@search_query}' on Alpha Vantage. Please check the ticker symbol."
+          @stocks = []
+        end
+      end
+    else
+      # Show popular stocks if no search
+      popular_tickers = %w[AAPL MSFT GOOGL AMZN TSLA META NFLX NVDA AMD INTC CRM ORCL IBM DIS BA]
+      @stocks = Stock.where(ticker: popular_tickers).order(:ticker)
+      
+      # Fetch missing popular stocks
+      missing_tickers = popular_tickers - @stocks.pluck(:ticker)
+      missing_tickers.each do |ticker|
+        stock = Stock.update_or_create_from_api(ticker)
+        @stocks = (@stocks.to_a + [stock]).compact if stock
+      end
+    end
+    
+    @stocks = @stocks.to_a.uniq(&:ticker)
   end
 
   def show
     @stock = Stock.find_by(ticker: params[:id].upcase)
+    @user_stock = current_user.user_stocks.find_by(stock: @stock) if @stock
 
     unless @stock
       @stock = Stock.update_or_create_from_api(params[:id])
@@ -13,24 +54,63 @@ class StocksController < ApplicationController
   end
 
   def create
-    ticker = params[:ticker].upcase
+    ticker = params[:ticker]&.upcase
     
     if ticker.blank?
       redirect_to stocks_path, alert: "Please enter a stock ticker symbol"
       return
     end
     
-    @stock = Stock.update_or_create_from_api(ticker)
+    # Check if user already tracks this stock
+    existing_stock = Stock.find_by(ticker: ticker)
+    if existing_stock && current_user.stocks.include?(existing_stock)
+      redirect_to stocks_path, alert: "You are already tracking #{ticker}"
+      return
+    end
+    
+    # Fetch or create the stock
+    @stock = existing_stock || Stock.update_or_create_from_api(ticker)
 
     if @stock
+      # Add to user's portfolio
+      current_user.user_stocks.find_or_create_by(stock: @stock)
       redirect_to stock_path(@stock.ticker), notice: "#{ticker} added to your portfolio!"
     else
       redirect_to stocks_path, alert: "Unable to fetch data for #{ticker}. Please check the ticker symbol and try again."
     end
   end
 
+  def track
+    @stock = Stock.find(params[:id])
+    
+    if current_user.stocks.include?(@stock)
+      redirect_to browse_stocks_path, alert: "You are already tracking #{@stock.ticker}"
+    else
+      current_user.user_stocks.create(stock: @stock)
+      redirect_to browse_stocks_path, notice: "#{@stock.ticker} added to your portfolio!"
+    end
+  end
+
+  def untrack
+    @user_stock = current_user.user_stocks.find_by(stock_id: params[:id])
+    
+    if @user_stock
+      ticker = @user_stock.stock.ticker
+      @user_stock.destroy
+      redirect_to stocks_path, notice: "#{ticker} removed from your portfolio"
+    else
+      redirect_to stocks_path, alert: "Stock not found in your portfolio"
+    end
+  end
+
   def update
     @stock = Stock.find_by(ticker: params[:id].upcase)
+    
+    # Check if user tracks this stock
+    unless current_user.stocks.include?(@stock)
+      redirect_to stocks_path, alert: "You are not tracking this stock"
+      return
+    end
     
     if @stock
       updated_stock = @stock.update_price!
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  resources :user_stocks
   devise_for :users
   
   # Health check endpoint
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Main application routes
-  resources :stocks, only: [:index, :show, :create, :update]
+  resources :stocks, only: [:index, :show, :create, :update] do
+    collection do
+      get :browse
+    end
+    member do
+      post :track
+      delete :untrack
+    end
+  end
+  
   root to: 'stocks#index'
 end
